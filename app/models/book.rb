79e42b1291b039f633c1fa364b8b738ea6402302
siweiwang <wangsiwei2003@hotmail.com>
2016-10-28 00:40:38 -0400

    add scope to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
+
+  scope :finished, ->{ where('finished_on IS not null') }
+  scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+
   def finished?
     finished_on.present?
   end
