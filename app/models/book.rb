3cd4f30dc373b2f3421b63b83c9a50588dbc4402
Siwei <swang@surfeasy.com>
2016-10-29 01:12:42 -0400

    implement search function

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,10 @@ class Book < ActiveRecord::Base
 
   scope :finished, ->{ where('finished_on IS not null') }
   scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
 
   def finished?
     finished_on.present?
   end
+
 end
