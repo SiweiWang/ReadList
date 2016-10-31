0004d7ebfede19ba802949350b66a1e2fea33bb9
Siwei <swang@surfeasy.com>
2016-10-31 00:58:59 -0400

    Optimization new books form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
