a52f1cc68576d4561347daa3d442f9eeb24b4f15
Siwei <swang@surfeasy.com>
2016-10-30 23:57:32 -0400

    make new book form able to create calls

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :description, :amazon_id)
+      params.require(:book).permit(:title, :author, :description, :amazon_id, { genre_ids: [] }, :rating)
     end
 end
